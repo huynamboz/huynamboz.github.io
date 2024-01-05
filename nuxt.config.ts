1520da7d0e5ad614b8c9f133adad47b16fe132f3
Trịnh Huy Nam <38585889+huynamboz@users.noreply.github.com>
2024-01-05 13:53:50 +0700

    chore: setup layout & tailwindCss (#1)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'layout', mode: 'out-in' },
+  },
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
 })
