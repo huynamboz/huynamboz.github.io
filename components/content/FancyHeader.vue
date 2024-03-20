e7b9ee4d41a1e03ae1eb434f225ec1e52514b47a
huynamboz <teahuynam2003@gmail.com>
2024-03-20 11:36:47 +0700

    feat: setup nuxt content

---
+++ components/content/FancyHeader.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2 class="fancy-header"><ContentSlot :use="$slots.default" unwrap="p" /></h2>
+</template>
---
+++ pages/[...slug].vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import 'github-markdown-css/github-markdown.css'
+</script>
+<template>
+  <main class="markdown-body">
+    <ContentDoc />
+  </main>
+</template>
