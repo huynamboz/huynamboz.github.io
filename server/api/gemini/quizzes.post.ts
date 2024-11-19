ce4aada41bfd63a739efc2287dd41271458354ab
Trịnh Huy Nam <38585889+huynamboz@users.noreply.github.com>
2024-11-19 21:49:16 +0700

    feat: generate quiz you gemini (#46)

---
+++ server/api/gemini/quizzes.post.ts
@@ -0,0 +1,49 @@
+import { z } from 'zod'
+import { sendTelegram } from '~/server/utils/telegram'
+
+const quizTypesSchema = z.enum(['TRUE_FALSE', 'MULTIPLE_CHOICE'])
+
+const quizSchema = z.object({
+  language: z.string().default('en'),
+  quizzes: z.array(z.string()).default([]),
+  option: z.object({
+    numberOfQuestion: z.number().default(1),
+    theme: z.string(),
+    quizTypes: z.array(quizTypesSchema),
+  }),
+})
+
+export default defineEventHandler(async (event) => {
+  // const { history, language, option } = await readBody(event)
+  try {
+    const result = await readValidatedBody(event, (body) => quizSchema.safeParse(body)) // or `.parse` to directly throw an error
+
+    if (!result.success) {
+      sendTelegram('Generate quiz: Validation failed', result.error)
+      throw errorHandler({ statusCode: 400, message: 'Validation failed' })
+    }
+
+    const { language, quizzes, option } = result.data
+
+    let responseText = (await generateQuiz(language, quizzes.join(','), option)) || ''
+    // handle remove ```json and ``` from responseText
+    responseText = responseText
+      ?.replace(/```json/g, '')
+      .replace(/```/g, '')
+      .trim()
+    if (!responseText) {
+      sendTelegram('Generate quiz: Unknown error' + responseText)
+      throw errorHandler({ statusCode: 400, message: 'Generate quiz error' })
+    }
+
+    const res = JSON.parse(responseText)
+    sendTelegram(
+      `Generate quiz: ${res.length} questions`,
+      res.map((q: any) => q.content),
+    )
+    return res
+  } catch (error: any) {
+    await sendTelegram(`Generate quiz: ${error?.message || 'Unknown error'}`)
+    throw errorHandler({ statusCode: 400, message: error?.message || 'Unknown error' })
+  }
+})
--- server/utils/exception.ts
+++ server/utils/exception.ts
@@ -2,6 +2,6 @@ import { type H3Error, createError } from 'h3'
 export const errorHandler = (error: { statusCode: number; message: string }): H3Error<unknown> => {
   return createError({
     statusCode: error.statusCode,
-    statusMessage: error.message,
+    message: error.message,
   })
 }
