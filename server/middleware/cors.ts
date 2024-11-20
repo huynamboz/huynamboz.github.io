63379131a4adc6b4fa490c781a1cedf47eaa204e
Mac <teahuynam2003@gmail.com>
2024-11-20 14:54:46 +0700

    feat: add response header

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,8 +1,10 @@
 export default defineEventHandler((event) => {
   setResponseHeaders(event, {
-    'Access-Control-Allow-Origin': '*', // Cho phép tất cả các nguồn gốc
-    'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Expose-Headers': '*',
   })
 
   // Xử lý các yêu cầu OPTIONS
