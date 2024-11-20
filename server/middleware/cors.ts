b0b2abd20b33cc5e411ef5dfcec2c4f09908177a
Mac <teahuynam2003@gmail.com>
2024-11-20 16:55:30 +0700

    fix: cors

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,14 +1,12 @@
+/**
+ * Middleware to respond CORS preflight requests using OPTIONS method.
+ */
 export default defineEventHandler((event) => {
-  setResponseHeaders(event, {
-    'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': 'true',
-    'Access-Control-Allow-Headers': '*',
-    'Access-Control-Expose-Headers': '*',
-  })
-  if (event.method === 'OPTIONS') {
+  // Answers HTTP 204 OK to CORS preflight requests using OPTIONS method :
+  // if (event.method === 'OPTIONS' && isPreflightRequest(event)) {
+  if (isPreflightRequest(event)) {
     event.node.res.statusCode = 204
-    event.node.res.statusMessage = 'No Content.'
+    event.node.res.statusMessage = 'No Content'
     return 'OK'
   }
 })
