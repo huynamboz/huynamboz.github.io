8c9c21fcf6932825dc41e221b65d71e538dd2c5d
Mac <teahuynam2003@gmail.com>
2024-11-22 00:12:00 +0700

    fix: full config cors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,21 @@ export default defineNuxtConfig({
     },
   ],
 
+  routeRules: {
+    '/api/**': {
+      // enable CORS
+      cors: true, // if enabled, also needs cors-preflight-request.ts Nitro middleware to answer CORS preflight requests
+      headers: {
+        // CORS headers
+        'Access-Control-Allow-Origin': '*', // 'http://example:6006', has to be set to the requesting domain that you want to send the credentials back to
+        'Access-Control-Allow-Methods': '*', // 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Headers': '*', // 'Origin, Content-Type, Accept, Authorization, X-Requested-With'
+        'Access-Control-Expose-Headers': '*',
+        // 'Access-Control-Max-Age': '7200', // 7200 = caching 2 hours (Chromium default), https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Max-Age#directives
+      },
+    },
+  },
   site: {
     url: 'https://hinam.nuxt.dev',
   },
--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -12,4 +12,10 @@ export default defineEventHandler((event) => {
     event.node.res.statusMessage = 'No Content.'
     return 'OK'
   }
+
+  if (isPreflightRequest(event)) {
+    event.node.res.statusCode = 204
+    event.node.res.statusMessage = 'No Content'
+    return 'OK'
+  }
 })
