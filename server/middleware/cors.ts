a42811d62b30f99e5d374b6b41612c64607686e0
Mac <teahuynam2003@gmail.com>
2024-11-22 00:22:31 +0700

    check  preflight

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,7 +1,6 @@
 export default defineEventHandler((event) => {
   // Answers HTTP 204 OK to CORS preflight requests using OPTIONS method :
-  // if (event.method === 'OPTIONS' && isPreflightRequest(event)) {
-  if (isPreflightRequest(event)) {
+  if (event.method === 'OPTIONS' && isPreflightRequest(event)) {
     event.node.res.statusCode = 204
     event.node.res.statusMessage = 'No Content'
     return 'OK'
