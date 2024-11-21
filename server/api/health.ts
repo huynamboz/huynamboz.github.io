6dc4d678edc99481ecb9cffca3cb7f6252c2882e
Mac <teahuynam2003@gmail.com>
2024-11-22 00:03:01 +0700

    fix: allow cors health api

--- server/api/health.ts
+++ server/api/health.ts
@@ -1,4 +1,11 @@
-export default defineEventHandler(() => {
+export default defineEventHandler((event) => {
+  setResponseHeaders(event, {
+    'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Expose-Headers': '*',
+  })
   return {
     status: 'healthy',
   }
