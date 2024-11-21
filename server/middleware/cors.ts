53894821fa027c94f14293a94a9518549a8b6ef4
Mac <teahuynam2003@gmail.com>
2024-11-21 23:03:00 +0700

    chore: change event res

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -6,9 +6,9 @@ export default defineEventHandler((event) => {
     'Access-Control-Allow-Headers': '*',
     'Access-Control-Expose-Headers': '*',
   })
-  if (getMethod(event) === 'OPTIONS') {
-    event.res.statusCode = 204
-    event.res.statusMessage = 'No Content.'
+  if (event.method === 'OPTIONS') {
+    event.node.res.statusCode = 204
+    event.node.res.statusMessage = 'No Content.'
     return 'OK'
   }
 })
