979bd757b93695ac77abb133c9a0e5b7300d776a
huynamboz <teahuynam2003@gmail.com>
2024-12-29 19:27:13 +0700

    feat: log

--- server/plugins/webhooks.ts
+++ server/plugins/webhooks.ts
@@ -9,7 +9,8 @@ interface WebhookData {
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('webhooks:call', async (data: WebhookData) => {
     const { data: webhookData, webhookUrls } = data
-    console.log('Webhook executing --------------->>>', webhookData)
+    console.log('Webhook executing --------------->>>', webhookUrls)
+    nitroApp.hooks.callHook('telegram', webhookUrls.join(','))
     try {
       await Promise.all(
         webhookUrls.map((url) =>
@@ -21,6 +22,7 @@ export default defineNitroPlugin((nitroApp) => {
             },
           }).catch((error) => {
             console.error(`Error with URL ${url}:`, error)
+            nitroApp.hooks.callHook('telegram', 'Webhook error', error)
             return null // Trả về null nếu có lỗi để không làm reject Promise.all
           }),
         ),
